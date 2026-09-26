0e685cae52b71e74df4db6e577b60c2aec75423f
snkpcn <yc6hdt6g2n@privaterelay.appleid.com>
2026-09-26 16:53:08 +0700

    test: advance semantic version contract to v30

--- tests/human-brain-semantic-v14-last-two-chunk1.test.ts
+++ tests/human-brain-semantic-v14-last-two-chunk1.test.ts
@@ -14,7 +14,7 @@ function byId(id:string){
 }
 
 test('semantic-v28 version is explicit',()=>{
-  assert.equal(SEMANTIC_INTERPRETER_VERSION, 'semantic-v29');
+  assert.equal(SEMANTIC_INTERPRETER_VERSION, 'semantic-v30');
 });
 
 test('semantic-v28 keeps journey-02 and informational-02 gold unchanged',()=>{
